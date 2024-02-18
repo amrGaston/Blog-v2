23f3f0b2260be13fee7cc96c217ea9df340424bf
Gaston Amurrio <elgaston011@gmail.com>
2024-02-18 16:30:43 -0300

    Agrego test para los gets

---
+++ src/test/java/com/blog/blog/controller/ComentarioControllerTest.java
@@ -0,0 +1,82 @@
+package com.blog.blog.controller;
+
+import com.blog.blog.controller.models.ComentarioResponseModel;
+import com.blog.blog.exception.ComentarioNoEncontradoException;
+import com.blog.blog.model.Comentario;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ComentarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ComentarioController comentarioController = new ComentarioController();
+    private final ComentarioController comentarioControllerMock = Mockito.mock(ComentarioController.class);
+    @BeforeEach
+    void setUp() throws ComentarioNoEncontradoException {
+        Comentario comentario = new Comentario();
+        comentario.setTexto("Un Comentario");
+        comentario.setId(1L);
+        ComentarioResponseModel comentarioResponseModelMock = new ComentarioResponseModel(comentario);
+        Mockito.when(comentarioControllerMock.getComentario(1L) ).thenReturn(comentarioResponseModelMock);
+    }
+
+    @Test
+    void getComentarioDevuelveComentarioConAtributoTexto() throws ComentarioNoEncontradoException {
+        ComentarioResponseModel comentarioResponseModel = comentarioControllerMock.getComentario(1L);
+        Assertions.assertEquals("Un Comentario",comentarioResponseModel.getTexto());
+    }
+
+    @Test
+    void getComentarioDevuelveComentarioConAtributoID() throws ComentarioNoEncontradoException {
+        ComentarioResponseModel comentarioResponseModel = comentarioControllerMock.getComentario(1L);
+        Assertions.assertEquals(1L,comentarioResponseModel.getId());
+    }
+
+    @Test
+    void getComentarioDevuelveRespuestaJson() throws ComentarioNoEncontradoException {
+        ComentarioResponseModel comentarioResponseModel = comentarioControllerMock.getComentario(1L);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "";
+        try {
+            json = objectMapper.writeValueAsString(comentarioResponseModel);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(validarFormatoJSON(json));
+    }
+
+    private boolean validarFormatoJSON(String jsonString) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+            return jsonNode.isObject();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //@Test
+    public void getComentarioDevuelveRespuesta200() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(comentarioController).build();
+        mockMvc.perform(get("/comentario/1")).andExpect(status().isOk() );
+
+    }
+}
---
+++ src/test/java/com/blog/blog/controller/UsuarioControllerTest.java
@@ -0,0 +1,83 @@
+package com.blog.blog.controller;
+
+import com.blog.blog.controller.models.UsuarioResponseModel;
+import com.blog.blog.exception.UsuarioNoEncontradoException;
+import com.blog.blog.model.Usuario;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final UsuarioController usuarioController = new UsuarioController();
+    private final UsuarioController usuarioControllerMock = Mockito.mock(UsuarioController.class);
+    @BeforeEach
+    void setUp() throws UsuarioNoEncontradoException {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Gastón");
+        usuario.setCorreo("gaston@gmail.com");
+        usuario.setPosts(new ArrayList<>());
+
+        UsuarioResponseModel usuarioResponseModelMock = new UsuarioResponseModel(usuario);
+        Mockito.when(usuarioControllerMock.getUsuario(1L) ).thenReturn(usuarioResponseModelMock);
+    }
+
+    @Test
+    void getUsuarioDevuelveUsuarioConAtributoNombre() throws UsuarioNoEncontradoException {
+        UsuarioResponseModel usuarioResponseModel = usuarioControllerMock.getUsuario(1L);
+        Assertions.assertEquals("Gastón",usuarioResponseModel.getNombre());
+    }
+
+    @Test
+    void getUsuarioDevuelveUsuarioConAtributoID() throws UsuarioNoEncontradoException {
+        UsuarioResponseModel usuarioResponseModel = usuarioControllerMock.getUsuario(1L);
+        Assertions.assertEquals(1L,usuarioResponseModel.getId());
+    }
+
+    @Test
+    void getUsuarioDevuelveUsuarioConAtributoCorreo() throws UsuarioNoEncontradoException {
+        UsuarioResponseModel usuarioResponseModel = usuarioControllerMock.getUsuario(1L);
+        Assertions.assertEquals("gaston@gmail.com",usuarioResponseModel.getCorreo());
+    }
+
+    @Test
+    void getUsuarioDevuelveRespuestaJson() throws UsuarioNoEncontradoException {
+        UsuarioResponseModel usuarioResponseModel = usuarioControllerMock.getUsuario(1L);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "";
+        try {
+            json = objectMapper.writeValueAsString(usuarioResponseModel);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(validarFormatoJSON(json));
+    }
+    private boolean validarFormatoJSON(String jsonString) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+            return jsonNode.isObject();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
